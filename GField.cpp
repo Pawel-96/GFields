7bce3f229f06b51bb98f2d086c175e2c9bb0e1d3
Pawel <paweldro1996@gmail.com>
2025-06-10 11:38:42 +0200

    Update

--- GField.cpp
+++ GField.cpp
@@ -1,3 +1,27 @@
+//****************************************************************************************//
+//                                                                                        //
+// Copyright 2025 Paweł Drozda                                                            //
+//                                                                                        //
+// This file is part of GField.                                                           //
+//                                                                                        //
+//To the extent possible under law, the author has dedicated this software	          //
+// to the public domain under the Creative Commons CC0 1.0 Universal License.             //
+//                                                                                        //
+// GField is distributed in the hope that it will be useful, but                          //
+// WITHOUT ANY WARRANTY; without even the implied warranty of                             //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                                   //
+//                                                                                        //
+// You should have received a copy of the CC0-1.0 License                                 //
+// along with GField.  If not, see <ttps://creativecommons.org/publicdomain/zero/1.0/>.   //
+//                                                                                        //
+//                                                                                        //
+//****************************************************************************************//
+
+
+
+
+
+
 #include "Incld.h"
 
 
